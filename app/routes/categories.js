4c5484edb608aa5891976cfc702232f55195e701
Arthur <arthur.boyreau@gmail.com>
2021-03-08 14:07:59 +0100

    Ajout de boutons pour ajouter des catégories et des contacts TODO : Modification et suppression des contacts et des catégories TODO : Affichage des contacts de la première catégorie à l'ouverture de la page TODO : Actualisation des contacts affichés après ajout d'un nouveau contact

--- app/routes/categories.js
+++ app/routes/categories.js
@@ -1,9 +1,25 @@
 import Route from '@ember/routing/route';
+import { action } from '@ember/object';
 
 export default class CategoriesRoute extends Route {
+  addContactDisplay = true;
+
   model() {
     let store = this.get('store');
     let categories = store.findAll('category');
     return categories;
+  }
+
+  @action willTransition(transition) {
+    console.log(transition.from.name);
+    if(transition.from.name == 'categories.index')
+      this.addContactDisplay = true;
+  }
+
+
+  @action addCategory() {
+    if(this.addContactDisplay) this.transitionTo('categories.add');
+    else this.transitionTo('categories');
+    this.addContactDisplay = !this.addContactDisplay;
   }
 }
--- app/routes/categories/contacts.js
+++ app/routes/categories/contacts.js
@@ -1,6 +1,8 @@
 import Route from '@ember/routing/route';
+import { action } from '@ember/object';
 
 export default class CategoriesContactsRoute extends Route {
+  addContactDisplay = true;
 
   async model(params) {
     let store = this.get('store');
@@ -8,5 +10,27 @@ export default class CategoriesContactsRoute extends Route {
     var contact = contacts.filter(contact => contact.category === this.paramsFor('categories/contacts').category_id);
 
     return contact;
+  }
+
+  @action willTransition(transition) {
+    console.log(transition.from.name);
+    if(transition.from.name === 'categories.contacts.add');
+      this.addContactDisplay = true;
+
+    console.log("Après transition : " + this.addContactDisplay);
+  }
+
+  @action addContact() {
+    console.log(this.addContactDisplay)
+    if(this.addContactDisplay)
+    {
+      this.transitionTo('categories.contacts.add');
+      this.addContactDisplay = false;
+    }
+    else
+    {
+      this.transitionTo('categories.contacts');
+      this.addContactDisplay = true;
+    }
   }
 }
