fdbc713fe981b6efd8ae96dcb09c00fecbdcbc58
Arthur <arthur.boyreau@gmail.com>
2021-03-18 02:32:35 +0100

    Amélioration du component Section-ui (il est pas beau mais au moins ça fonctionne)

--- app/routes/sections.js
+++ app/routes/sections.js
@@ -2,16 +2,13 @@ import Route from '@ember/routing/route';
 
 export default class SectionsRoute extends Route {
   store;
-  sections = [];
 
   model() {
     this.store = this.get('store');
-    this.store.findAll('section').then(sec => {
-      this.sections.push(sec);
-      console.log(sec);
-    });
-
-    console.log(this.sections);
-    return this.sections;
+    var sections = this.store.findAll('section');
+    sections.then(value => {
+      console.log(value);
+    })
+    return sections;
   }
 }
