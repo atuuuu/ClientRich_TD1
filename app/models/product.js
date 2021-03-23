93f37ad9501df60dc97e53d07bfb92ef75339fbd
Arthur <arthur.boyreau@gmail.com>
2021-03-23 01:34:46 +0100

    Ajout d'un component product-view et intégration de celui-ci dans la page produit

--- app/models/product.js
+++ app/models/product.js
@@ -3,6 +3,8 @@ import Model, { attr, belongsTo, hasMany } from '@ember-data/model';
 export default class ProductModel extends Model {
   @attr('string') name;
   @attr('string') price;
+  @attr('string') image;
+  @attr('string') comment;
   @belongsTo('section') section;
   @hasMany('product', { inverse: null }) packs;
 }
--- app/routes/products.js
+++ app/routes/products.js
@@ -2,4 +2,12 @@ import Route from '@ember/routing/route';
 import Abstractroute from "./Abstractroute";
 
 export default class ProductsRoute extends Abstractroute {
+
+  model() {
+    var model = {};
+    var prod = this.get('store').findAll('product');
+
+    model.product = prod;
+    return prod;
+  }
 }
